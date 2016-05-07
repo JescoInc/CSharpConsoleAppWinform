ac6ae3e3a01cc1f2992cfa867809ea783a3dace4
Jesse Glover <silverdrakeproductions@gmail.com>
2016-05-06 21:25:45 -0400

    Shows how to load an empty Winform from within a Console Application in C# instead of F#.

---
+++ C#ConsoleWinForm/ConsoleWinForm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms; // add DLL to System.Windows.Forms
+
+namespace ConsoleWinForm
+{
+    internal static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        internal static extern Boolean AllocConsole();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                // run as windows app
+                Application.EnableVisualStyles();
+                Application.Run(new Form());
+            }
+
+            else
+            {
+                // run as console app
+                NativeMethods.AllocConsole();
+                Console.WriteLine("Hello World");
+                Console.ReadLine();
+            }
+        }
+    }
+}
